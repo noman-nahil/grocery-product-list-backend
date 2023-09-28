163b735c69dbcaa7d95a9774e4b023738d526532
noman-nahil <abdullahnoman1997@gmail.com>
2023-09-28 11:42:38 +0600

    Added new product to the database

--- routes/product.js
+++ routes/product.js
@@ -5,5 +5,17 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Product List");
 });
-
+router.post("/", (req, res) => {
+  const { name, price, description, link } = req.body;
+  if (name !== "" && price !== "" && description !== "" && link !== "") {
+    //console.log(req.body);
+    const sql = `INSERT INTO product_list (name, price, description, link) VALUES ('${name}', ${price}, '${description}', '${link}')`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.json({ message: "Item added" });
+    });
+  } else {
+    console.log("Something is missing");
+  }
+});
 module.exports = router;
