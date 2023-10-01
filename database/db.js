3eaa17c95bf6d8a12e64f31b382912664663393d
noman-nahil <abdullahnoman1997@gmail.com>
2023-10-02 00:00:51 +0600

    Fixes some issues

--- database/db.js
+++ database/db.js
@@ -1,21 +1,26 @@
 const mysql = require("mysql");
 
-let db;
-if (process.env.JAWSDB_URL) {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-  });
-} else {
-  db = mysql.createConnection({
-    host: "127.0.0.1",
-    database: "grocery-product",
-    user: "root",
-    password: "",
-  });
-}
+let db = (db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+}));
+// if (process.env.JAWSDB_URL) {
+//   db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+//   });
+// } else {
+//   db = mysql.createConnection({
+//     host: "127.0.0.1",
+//     database: "grocery-product",
+//     user: "root",
+//     password: "",
+//   });
+// }
 
 // Connect to the database
 db.connect((error) => {
