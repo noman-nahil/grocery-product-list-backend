8b18a39bbdadd3a2bd503724fbbe62d36ff6a60d
noman-nahil <abdullahnoman1997@gmail.com>
2023-10-01 23:16:09 +0600

    heroku

--- database/db.js
+++ database/db.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: "127.0.0.1",
-  database: "grocery-product",
-  user: "root",
-  password: "",
-});
+let db;
+if (process.env.JAWSDB_URL) {
+  db = mysql.createConnection(process.env.JAWSDB_UR);
+} else {
+  db = mysql.createConnection({
+    host: "127.0.0.1",
+    database: "grocery-product",
+    user: "root",
+    password: "",
+  });
+}
 
 // Connect to the database
 db.connect((error) => {
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const product = require("./routes/product");
 
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/product", product);
 app.get("/", (req, res) => {
